2456364b3b1d7daa12de1a4b17846e9230ad5802
Chakradhar Bandla <chakrabandla@gmail.com>
2020-07-08 12:49:13 +0100

    filebeat

---
+++ src/main/java/uk/ac/ebi/pride/archive/repo/ws/config/SimpleFilter.java
@@ -0,0 +1,30 @@
+package uk.ac.ebi.pride.archive.repo.ws.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
+public class SimpleFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if (!request.getRequestURL().toString().contains("actuator/health")) {
+            log.info("Request URL: " + request.getRequestURL());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
